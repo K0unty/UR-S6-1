78f8dabf42fe805caf13f3e01d9b33bb5424e800
K0unty <kountiapi@pm.me>
2024-07-20 22:41:47 +0530

    addb

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
-import logo from '../assets/logo.png';
+import logo from "../assets/lo.gif";
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
+      <h1>ReactPanty</h1>
       <p>A community of artists and art-lovers.</p>
     </header>
   );
