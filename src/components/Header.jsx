e27030cfd1492a51176ba4d032c5c5af87243bb6
K0unty <kountiapi@pm.me>
2024-07-22 21:45:22 +0530

    addingTAilwindCSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -50,16 +50,16 @@ import logo from "../assets/lo.gif";
 
 export default function Header() {
   return (
-    <header className="flex flex-col items-center mt-8 mb-16">
+    <header className="flex flex-col items-center mt-8 mb-8 md:mb-16">
       <img
         src={logo}
         alt="A canvas"
         className="mb-8 w-44 h-44 object-contain"
       />
-      <h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title">
+      <h1 className="text-xl md:text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title">
         ReactPanty
       </h1>
-      <p className="text-red-500">A community of ass and pussy-lovers.</p>
+      <p className="text-lime-500">A community of ass and pussy-lovers.</p>
     </header>
   );
 }
