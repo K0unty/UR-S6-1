d00a9e548a5a155424beb7a25186999debbe1274
K0unty <kountiapi@pm.me>
2024-07-22 21:26:12 +0530

    restylewithTailwindCSs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -51,9 +51,15 @@ import logo from "../assets/lo.gif";
 export default function Header() {
   return (
     <header className="flex flex-col items-center mt-8 mb-16">
-      <img src={logo} alt="A canvas" />
-      <h1>ReactPanty</h1>
-      <p>A community of artists and art-lovers.</p>
+      <img
+        src={logo}
+        alt="A canvas"
+        className="mb-8 w-44 h-44 object-contain"
+      />
+      <h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title">
+        ReactPanty
+      </h1>
+      <p className="text-red-500">A community of ass and pussy-lovers.</p>
     </header>
   );
 }
