7415d0a351a83e0e8eea2700540ede95b8fa0873
K0unty <kountiapi@pm.me>
2024-07-22 15:21:09 +0530

    CssModulesSCoping

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import logo from "../assets/lo.gif";
-import "./Header.css";
+import classes from "./Header.module.css";
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactPanty</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p className={classes.paragraph}>
+        A community of artists and art-lovers.
+      </p>
     </header>
   );
 }
